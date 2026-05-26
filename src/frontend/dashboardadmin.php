716d0cacc3fe1fa41d8dc169d4ae4dc6e1c0c0db
Abaddon-the-despoiler <sammifr114@gmail.com>
2026-05-26 14:42:46 +0200

    2th commit

--- src/frontend/dashboardadmin.php
+++ src/frontend/dashboardadmin.php
@@ -1,4 +1,9 @@
+<?php
+require('classblog.php');
 
+// Hier wodt allen blogs opgehaald uit de database afhankelijk welke rol je hebt. 
+
+?>
 
 <html>
 
@@ -27,17 +32,22 @@
 <button type="button">Nieuwe blog aanmaken</button>
 </div>
  <div class="blogs">
-
+    <?php
+ $data1 = $_Blog->readBlog();
+foreach($data1 as $row){?>
 
 <table class="overzicht">
-
+<td><h3> <?php echo $row['titel'];  ?> </h3> </td>
+<td> <button><a href="update.php?id=<? echo $row['id']; ?>">Update</a></button></td>
+<td><button> <a href="delete.php?id=<? echo $row['id']; ?>">Verwijderen</a></button></td>
 </table>
 
 
 
+<?php
+}
 
-
-
+?>
 </div>
 </body>
 

9faf4f23405ae43dd61c661249129c5379421c68
Abaddon-the-despoiler <sammifr114@gmail.com>
2026-05-26 14:40:40 +0200

    1th commit

---
+++ src/frontend/dashboardadmin.php
@@ -0,0 +1,48 @@
+
+
+<html>
+
+
+<head>
+<link rel="stylesheet" href="styles/dasboardadmin.css">
+</head>
+    
+<body>
+
+<nav>
+    <img src="images/logo.png" alt="logo vacantie blog">
+
+   
+        <ul>
+        <li><a href="contact.asp">Username</a></li>
+        <li><button type="submit">Loguit</button></li>
+        </ul>
+
+</nav>
+
+
+
+
+<div class="buttons">
+<button type="button">Nieuwe blog aanmaken</button>
+</div>
+ <div class="blogs">
+
+
+<table class="overzicht">
+
+</table>
+
+
+
+
+
+
+</div>
+</body>
+
+
+
+
+
+<html>
